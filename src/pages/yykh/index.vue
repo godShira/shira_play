07db45dac2a5d7619e1be9306c40d83f8ac222d6
hanqi.dan <1450036341@qq.com>
2020-12-25 10:54:50 +0800

    getAddressCoords

--- src/pages/yykh/index.vue
+++ src/pages/yykh/index.vue
@@ -5,9 +5,11 @@
 
 <script>
 import { getAddressCoords } from '@/use'
+import { groupArr } from '@/util/arr'
 export default {
   name: 'yykh',
   setup() {
+    groupArr()
     getAddressCoords('virtua_map', '北京上的积极健康的世界冠军考试大纲', res => {
       console.log(res)
     })
--- src/util/arr.js
+++ src/util/arr.js
@@ -19,6 +19,7 @@ export const findBy = (arr, value, prop = 'value', defaultIndex = -1) =>
  * @param {*} initid 默认首层 pid
  * 用法 treeData(list) or treeData(list, { pid: 'parentId' })
  */
+
 export const treeData = (data, opt) => {
   let { id = 'id', pid = 'pid', children = 'children', initid = 0 } = opt
   let cloneData = JSON.parse(JSON.stringify(data))
@@ -26,5 +27,34 @@ export const treeData = (data, opt) => {
     let branchArr = cloneData.filter(child => father[id] === child[pid])
     branchArr.length > 0 ? (father[children] = branchArr) : ''
     return father[pid] === initid
+  })
+}
+
+const groupBy = (list, key, key1) => {
+  const newList = list.reduce((prevValue, currentValue) => {
+    let index = -1
+    prevValue.some((item, i) => {
+      if (item[key] === currentValue[key]) {
+        index = i
+        return true
+      }
+    })
+    if (index > -1) {
+      prevValue[index].children.push(currentValue)
+    } else {
+      prevValue.push({
+        [key]: currentValue[key],
+        label: currentValue[key],
+        children: [{ ...currentValue, label: currentValue[key1] }]
+      })
+    }
+    return prevValue
+  }, [])
+  return newList
+}
+export const groupArr = () => {
+  import('./all.json').then(res => {
+    const list = res.data
+    console.log(groupBy(list, 'province', 'city'))
   })
 }
